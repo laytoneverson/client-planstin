5a594abbe3488bcf026b018366f0a42b98f5b3df
Layton E <layton.everson@gmail.com>
2019-01-21 14:25:18 -0700

    wip - migrate member plans

--- app/Jobs/CreateMemberPlanEnrollments.php
+++ app/Jobs/CreateMemberPlanEnrollments.php
@@ -2,24 +2,32 @@
 
 namespace App\Jobs;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use stdClass;
 
 class CreateMemberPlanEnrollments implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     /**
+     * @var stdClass
+     */
+    private $memberRecord;
+
+    /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($memberRecord)
     {
-        //
+
+        $this->memberRecord = $memberRecord;
     }
 
     /**
@@ -27,8 +35,8 @@ class CreateMemberPlanEnrollments implements ShouldQueue
      *
      * @return void
      */
-    public function handle()
+    public function handle(EntityManagerInterface $entityManager)
     {
-        //
+
     }
 }
